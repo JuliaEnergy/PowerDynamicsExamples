4f3db4a1d30f90f9cbeddb6301e236984ea442eb
SabineAuer <sabine.auer@elena-international.com>
2019-08-14 16:30:35 +0200

    parameterization of IEEE 14 bus system corrected (#6)
    * parameterization of IEEE 14 bus system corrected
    
    * inertia does not need to be scaled by factor 2 since this missing scaling factor is now included in the model equations directly

--- ieee14-minimal/runexample.jl
+++ ieee14-minimal/runexample.jl
@@ -6,8 +6,10 @@ powergrid = read_powergrid("ieee-14-minimal.json", Json)
 
 operationpoint = find_operationpoint(powergrid)
 
-result = simulate(Perturbation(1, :ω, Inc(0.2)), powergrid, operationpoint, timespan = (0.0,0.3))
-plot_res(result, powergrid)
+result = simulate(Perturbation(1, :ω, Inc(0.2)), powergrid, operationpoint, timespan = (0.0,10.))
+pl = plot_res(result, powergrid)
+#savefig(pl,"ieee14-minimal-omega_perturbation.pdf")
 
-result = simulate(LineFault(from=1,to=5), powergrid, operationpoint, timespan = (0.0,1.0))
-plot_res(result, powergrid)
+result = simulate(LineFault(from=1,to=5), powergrid, operationpoint, timespan = (0.0,10.0))
+pl= plot_res(result, powergrid)
+#savefig(pl, "ieee14-minimal-line-fault.pdf")
