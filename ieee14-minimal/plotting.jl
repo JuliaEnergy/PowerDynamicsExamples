d1cd51181e4b155fa494b6a65d9a63f681ce63d9
Jan Lisse <jan.lisse@posteo.de>
2019-08-15 16:41:48 +0200

    Fix minimal example & align with 4th order example

--- ieee14-minimal/plotting.jl
+++ ieee14-minimal/plotting.jl
@@ -1,15 +1,15 @@
-using PowerDynamics
-using LaTeXStrings
-using Plots
+using PowerDynamics: symbolsof
+using Plots: plot, get_color_palette, plot_color
+using LaTeXStrings: latexstring, @L_str
 
-function plot_res(result, powergrid)
-    swing_indices = findall(n -> isa(n, SwingEqLVS), powergrid.nodes)
-    ω_colors = reshape(Plots.get_color_palette(:auto, plot_color(:white), 8)[swing_indices], (1,length(swing_indices)))
+function create_plot(sol)
+    swing_indices = findall(n -> :ω ∈ symbolsof(n), sol.powergrid.nodes)
+    ω_colors = reshape(get_color_palette(:auto, plot_color(:white), 8)[swing_indices], (1,length(swing_indices)))
     ω_labels = reshape([latexstring(string(raw"\omega", "_{$i}")) for i=swing_indices], (1, length(swing_indices)))
-    p_labels = reshape([latexstring(string(raw"p", "_{$i}")) for i=1:length(powergrid.nodes)], (1, length(powergrid.nodes)))
+    p_labels = reshape([latexstring(string(raw"p", "_{$i}")) for i=1:length(sol.powergrid.nodes)], (1, length(sol.powergrid.nodes)))
 
-    pl_p = plot(result, :, :p, legend = (0.8, 0.95), ylabel=L"p [p.u.]", label=p_labels)
-    pl_ω = plot(result, swing_indices, :ω, legend = (0.8, 0.7), ylabel=L"\omega \left[rad/s\right]", label=ω_labels, color=ω_colors)
+    pl_p = plot(sol, :, :p, legend = (0.8, 0.95), ylabel=L"p [p.u.]", label=p_labels)
+    pl_ω = plot(sol, swing_indices, :ω, legend = (0.8, 0.7), ylabel=L"\omega \left[rad/s\right]", label=ω_labels, color=ω_colors)
     pl = plot(
         pl_p, pl_ω;
         layout=(2,1),
@@ -17,6 +17,5 @@ function plot_res(result, powergrid)
         lw=3,
         xlabel=L"t[s]"
     )
-    display(pl)
-    pl
+    return pl
 end
--- ieee14-minimal/runexample.jl
+++ ieee14-minimal/runexample.jl
@@ -1,15 +1,17 @@
-using PowerDynamics: read_powergrid, Json, find_operationpoint, Perturbation, LineFault, simulate
-
+using PowerDynamics: read_powergrid, Json, find_operationpoint, Perturbation, Inc, LineFault, simulate
+using Plots: savefig
 include("plotting.jl")
 
 powergrid = read_powergrid("ieee-14-minimal.json", Json)
 
 operationpoint = find_operationpoint(powergrid)
 
-result = simulate(Perturbation(1, :ω, Inc(0.2)), powergrid, operationpoint, timespan = (0.0,10.))
-pl = plot_res(result, powergrid)
-#savefig(pl,"ieee14-minimal-omega_perturbation.pdf")
+# simulating a frequency perturbation at node 1
+solution1 = simulate(Perturbation(1, :ω, Inc(0.2)), powergrid, operationpoint, timespan = (0.0,10.))
+plot1 = create_plot(solution1)
+savefig(plot1,"ieee14-minimal-omega_perturbation.pdf")
 
-result = simulate(LineFault(from=1,to=5), powergrid, operationpoint, timespan = (0.0,10.0))
-pl= plot_res(result, powergrid)
-#savefig(pl, "ieee14-minimal-line-fault.pdf")
+# simulating a tripped line between node 1 and 5
+solution2 = simulate(LineFault(from=1,to=5), powergrid, operationpoint, timespan = (0.0,10.0))
+plot2 = create_plot(solution2)
+savefig(plot2, "ieee14-minimal-line-fault.pdf")
