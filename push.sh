04e6cbaa56126e8a0e8201d6f7da73998a9d107f
Jan Lisse <jan.lisse@posteo.de>
2019-08-21 15:58:45 +0200

    Make sure to only commit notebook file type

--- push.sh
+++ push.sh
@@ -10,7 +10,8 @@ if ! git diff-index --quiet HEAD --; then
     echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
 
     echo "Pushing updated notebooks"
-    git commit -a -m "Automatic update of notebooks [skip ci]" # skip ci prevents infinite triggering of travis
-    git show –name-only
+    git add *.ipynb
+    git commit -m "Automatic update of notebooks [skip ci]" # skip ci prevents infinite triggering of travis
+    git diff-tree --no-commit-id --name-only -r HEAD # list changed files
     git push origin HEAD:$TRAVIS_BRANCH
 fi
