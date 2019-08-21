5f116421909e04cb3ed3493603082a64752add1e
Jan Lisse <jan.lisse@posteo.de>
2019-08-21 12:23:31 +0200

    Make sure Literate package is available

--- generate_notebooks.sh
+++ generate_notebooks.sh
@@ -1,4 +1,4 @@
-julia --check-bounds=yes --color=yes -e "using Pkg; Pkg.activate(\"./notebooks\"); using Literate;
+julia --check-bounds=yes --color=yes -e "using Pkg; Pkg.activate(\"./notebooks\");Pkg.instantiate(); using Literate;
 Literate.notebook(\"ieee14-4th-order/runexample.jl\",\"ieee14-4th-order\",execute=false)
 Literate.notebook(\"ieee14-minimal/runexample.jl\",\"ieee14-minimal\",execute=false)
 "
