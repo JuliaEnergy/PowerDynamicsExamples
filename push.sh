b81db1b742ae43946dd00a0926d8d69c991dc7af
Jan Lisse <jan.lisse@posteo.de>
2019-08-21 15:23:18 +0200

    Pushing changes to github

--- push.sh
+++ push.sh
@@ -6,8 +6,7 @@ chmod 400 ./travis_key
 echo "Host github.com" > ~/.ssh/config
 echo "  IdentityFile $(pwd)/travis_key" >> ~/.ssh/config
 git remote set-url origin git@github.com:JuliaEnergy/PowerDynamicsExamples.git
+echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
 
-git diff --name-status
-# test connection
-git remote -v
-ssh -T git@github.com
+git commit -a -m "Update notebooks"
+git push -v
